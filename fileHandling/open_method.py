f8ccd12fca6bdeae48d8b83226f51b1bcde69d67
Akshay Darade <akshaydarade6675@gmail.com>
2025-03-31 14:52:15 +0000

    pattern practice

--- fileHandling/open_method.py
+++ fileHandling/open_method.py
@@ -2,23 +2,53 @@
 # how to open a file in python
 # open (filenmae,openmode)
 
-'''
-opening mode:
+# '''
+# opening mode:
 
-w: write mode (new file is created)
-r: read mode (new file won't be created)
-a: append mode (new file will be created)
+# w: write mode (new file is created)
+# r: read mode (new file won't be created)
+# a: append mode (new file will be created)
 
-'''
+# '''
 
 
-f=open("f1.txt","w")
-print('file opened')
-print('file is writable',f.writable())
-print('file is readable',f.readable())
-print('file name is',f.name)
-print('file is closed',f.closed)
-f.close()
+# f=open("f1.txt","w")
+# print('file opened')
+# print('file is writable',f.writable())
+# print('file is readable',f.readable())
+# print('file name is',f.name)
+# print('file is closed',f.closed)
+# f.close()
 
-print('file is closed',f.closed)
+# print('file is closed',f.closed)
+
+
+
+# q1.) create a txt file and and find out  f1.txt files in the words and charcters
+
+# f1=open("f1.txt","r")
+# print(f1.read())
+# f1.close()
+# # text=open("f1.txt","r").read()
+# word_count = len(text.split())
+# char_count = len(text)
+# print(word_count)
+# print(char_count)
+    
+import csv  
+    
+# Open the file "f1.txt" in read mode and read its content
+with open("f1.txt", "r") as f1:
+    text = f1.read()
+
+# Print the content of the file
+print(text)
+
+# Count the words and characters in the text
+word_count = len(text.split())
+char_count = len(text)
+
+# Print the word and character counts
+print("Word count:", word_count)
+print("Character count:", char_count)
 
---
+++ pattern/pattern_prac.py
@@ -0,0 +1,38 @@
+# n=int(input("enter the number: "))
+# print("* "*n)
+
+
+# n=int(input("enter the number: "))
+# for i in range(n):
+#     print("* "*n)
+
+
+# n=int(input("enter the number: "))
+# for i in range(n):
+#     print((str(i+1)+" ")*n)
+
+
+# n=int(input("enter the number: "))
+# for i in range(n):
+#     print((chr(i+65)+" ")*n)
+    
+   # reverse format
+# n = int(input("Enter the number: "))
+# for i in range(n-1, -1, -1):  # Start from n-1 and go down to 0
+#     print((str(i + 1) + " ") * n) 
+   
+   
+# n=int(input("enter the number: "))
+# for i in range(n):
+#     for j in range(n):
+#         print(j+1,end=" ")
+#     print()
+    
+    
+# reversed format
+# n=int(input("enter the number: "))
+# for i in range(n):
+#     for j in range(n-1,-1,-1):
+#         print(str(j+1),end=" "*n)
+#     print()
+
