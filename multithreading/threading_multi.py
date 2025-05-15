52279e6fd85a0f1561c1ac386d98d53052068b17
Akshay Darade <akshaydarade6675@gmail.com>
2025-05-15 14:09:47 +0000

    use join method

--- multithreading/threading_multi.py
+++ multithreading/threading_multi.py
@@ -47,4 +47,49 @@ from threading import *
 
 # datetime function
 
+import time
+import datetime
+from threading import *
 
+
+# def double(list):
+#     for i in list:
+#         print('double is ',i*2)
+#         time.sleep(2)
+        
+        
+# def sqare(list):
+#     for i in list:
+#         print('square is ',i*i)
+#         time.sleep(2)
+        
+# nums=[1,2,3,4,5,6,7,8,9,10]
+# begining_time=datetime.datetime.now()
+
+# t1=Thread(target=double,args=(nums,))
+# t2=Thread(target=sqare,args=(nums,))
+
+# t1.start()
+# t2.start()
+
+# t1.join()
+# t2.join()
+
+# print("total time: ",datetime.datetime.now()-begining_time)
+
+
+
+
+# join  method  
+def f1():
+    for i in range(10):
+        print("child thread")
+        time.sleep(2)
+        
+t1=Thread(target=f1)
+t1.start()
+t1.join()
+
+for i in range(10):
+    print("main thread")
+    time.sleep(2)
